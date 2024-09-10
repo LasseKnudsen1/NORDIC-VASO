37ab7573723b9a7b8bfe89738a9c98cbae514701
Lasse Knudsen <84593348+LasseKnudsen1@users.noreply.github.com>
2024-09-10 15:22:02 +0800

    Update call_findRightVersion.m
    Added updated percent signal change analysis (bar graph of mean PSC across voxels and runs.

--- manuscriptCode/call_findRightVersion.m
+++ manuscriptCode/call_findRightVersion.m
@@ -3,8 +3,8 @@
 %"preprocessed" everytime. Then run with runningMode="loadMode".
 
 % for version=["cwf" "cw" "wf" "cf" "c" "w" "f" "allOff"]
-for version=["wf"]
-clearvars -except version; clc; close all
+for version=["cwf"]
+clearvars -except version; close all
 ARG.rootDir='/Volumes/china2/nordicVASO/testNORDICvaso/';
 
 %Set adjustable arguments:
@@ -14,6 +14,8 @@ ARG.contrast='VASO';
 ARG.num_vol=144;
 ARG.num_TRperBlock=6;
 ARG.num_blocks=24; %Per run (twice the number of trials)
+ARG.discardFirstVols=2;
+ARG.runs=1:6;
 
 if ARG.runningMode=="loadMode"
     saveFigs=0; %put this to 1 if figures should be saved/overwritten
@@ -36,22 +38,23 @@ ARG.pPSCs_noNORDIC=[ARG.rootDir 'noNORDIC/analysis/noNORDIC_PSCs_' ARG.contrast 
 ARG.pdeltas_dif=[ARG.analysisDir '/dif/' ARG.version '_dif_deltas_BOLD.nii']; %Note, we only look at bold for difference delta.
 ARG.pdeltas_NORDIC_resample=[ARG.analysisDir '/' ARG.version '_deltas_' ARG.contrast '_resample.nii'];
 ARG.pdeltas_noNORDIC_resample=[ARG.rootDir 'noNORDIC/analysis/noNORDIC_deltas_' ARG.contrast '_resample.nii'];
-ARG.praw_magn_NORDIC=[ARG.resultsDir '/' ARG.version '_run1_' ARG.inversion '_magn.nii'];
-ARG.praw_magn_noNORDIC=[ARG.rootDir 'noNORDIC/noNORDIC_run1_' ARG.inversion '_magn.nii'];
-ARG.ptVal_NORDIC=[ARG.analysisDir '/' ARG.version '_t_' ARG.contrast '.nii'];
-ARG.ptVal_noNORDIC=[ARG.rootDir 'noNORDIC/analysis/noNORDIC_t_' ARG.contrast '.nii'];
-ARG.ptSNR_NORDIC=[ARG.resultsDir '/tSNR_' ARG.version '_run1_' ARG.inversion '.nii'];
-ARG.ptSNR_noNORDIC=[ARG.rootDir 'noNORDIC/tSNR_noNORDIC_run1_' ARG.inversion '.nii'];
 ARG.pROI=[ARG.rootDir 'ROI1.nii'];
 ARG.pdepthmap=[ARG.rootDir 'rim2_metric_equidist_Zcutup.nii'];
 
 
 cd(ARG.resultsDir)
-%For f-versions also set names of phase-files:
-if sum(ARG.version=='f') == 1
-    ARG.praw_phase_NORDIC=[ARG.resultsDir '/' ARG.version '_run1_' ARG.inversion '_phase.nii'];
-    ARG.praw_phase_noNORDIC=[ARG.rootDir 'noNORDIC/noNORDIC_run1_' ARG.inversion '_phase.nii'];
+
+%paths to motion corrected timeseries:
+for run = 1:numel(ARG.runs) %(made compatible with runs not starting from 1):
+    if ARG.contrast=="VASO"
+        ARG.pMoco_NORDIC{run}=[ARG.resultsDir '/' ARG.version '_run' num2str(ARG.runs(run)) '_VASO_LN.nii'];
+        ARG.pMoco_noNORDIC{run}=[ARG.rootDir 'noNORDIC/noNORDIC' '_run' num2str(ARG.runs(run)) '_VASO_LN.nii'];
+    elseif ARG.contrast=="BOLD"
+        ARG.pMoco_NORDIC{run}=[ARG.resultsDir '/' ARG.version '_moco_run' num2str(ARG.runs(run)) '_INV2_magn.nii'];
+        ARG.pMoco_noNORDIC{run}=[ARG.rootDir 'noNORDIC/noNORDIC' '_moco_run' num2str(ARG.runs(run)) '_INV2_magn.nii'];
+    end
 end
+
 
 %Run:
 if ARG.runningMode=="loadMode"
