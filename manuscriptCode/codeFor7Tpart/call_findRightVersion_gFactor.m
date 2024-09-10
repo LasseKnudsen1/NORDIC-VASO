46af5354d195e9a9dd9671552aa136f21da30302
Lasse Knudsen <84593348+LasseKnudsen1@users.noreply.github.com>
2024-09-10 15:24:00 +0800

    Update call_findRightVersion_gFactor.m
    Added updated percent signal change analysis (bar graph of mean PSC across voxels and runs.

--- manuscriptCode/codeFor7Tpart/call_findRightVersion_gFactor.m
+++ manuscriptCode/codeFor7Tpart/call_findRightVersion_gFactor.m
@@ -3,10 +3,10 @@
 %"preprocessed" everytime. Then run with runningMode="loadMode". If E.g.
 %ROI or depthmap changes then run again in saveMode first. 
 
-%for version=["wf" "w" "wf_gSame" "wf_gNearest"]
-for version=["w"]
+% for version=["wf" "w" "wf_gSame" "wf_gNearest"]
+for version=["wf_gNearest"]
 clearvars -except version; clc; close all
-ARG.rootDir='/Volumes/china2/nordicVASO/20230519_EMM/';
+ARG.rootDir='/Volumes/china2/nordicVASO/20230519_EMM/updated/';
 
 %Set adjustable arguments:d
 ARG.version=convertStringsToChars(version); 
@@ -15,6 +15,8 @@ ARG.contrast='VASO';
 ARG.num_vol=144;
 ARG.num_TRperBlock=6;
 ARG.num_blocks=24; %Per run (twice the number of trials)
+ARG.discardFirstVols=2;
+ARG.runs=3:6;
 
 if ARG.runningMode=="loadMode"
     saveFigs=0;
@@ -39,6 +41,16 @@ ARG.pdeltas_noNORDIC_resample=[ARG.rootDir 'w_noNORDIC/analysis/w_noNORDIC_delta
 ARG.pROI=[ARG.rootDir 'ROI1.nii'];
 ARG.pdepthmap=[ARG.rootDir 'rim2_metric_equidist.nii'];
 
+%paths to motion corrected timeseries:
+for run = 1:numel(ARG.runs) %(made compatible with runs not starting from 1):
+    if ARG.contrast=="VASO"
+        ARG.pMoco_NORDIC{run}=[ARG.resultsDir '/' ARG.version '_run' num2str(ARG.runs(run)) '_VASO_LN.nii'];
+        ARG.pMoco_noNORDIC{run}=[ARG.rootDir 'w_noNORDIC/w_noNORDIC' '_run' num2str(ARG.runs(run)) '_VASO_LN.nii'];
+    elseif ARG.contrast=="BOLD"
+        ARG.pMoco_NORDIC{run}=[ARG.resultsDir '/' ARG.version '_moco_run' num2str(ARG.runs(run)) '_INV2_magn.nii'];
+        ARG.pMoco_noNORDIC{run}=[ARG.rootDir 'w_noNORDIC/w_noNORDIC' '_moco_run' num2str(ARG.runs(run)) '_INV2_magn.nii'];
+    end
+end
 
 cd(ARG.resultsDir)
 
